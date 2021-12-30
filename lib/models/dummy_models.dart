6b9651d784c604a17990f38b53b865814a9344d4
gnihker <sadapa.chueng@gmail.com>
2021-12-31 01:40:48 +0700

    change model data structure

--- lib/models/dummy_models.dart
+++ lib/models/dummy_models.dart
@@ -1,42 +1,61 @@
 import './model.dart';
 
-const dummyModels = [
+final dummyModels = [
   Model(
-    label: 'Cat Dog classification',
-    method: 'POST',
-    route: 'http://10.0.2.2:5000/submit',
-    key: 'img',
-    description: 'Cat and Dog classification',
-  ),
+      detail: Detail(
+          description: "this is cat-dog classification model",
+          imgType: "file",
+          key: "img",
+          method: "POST",
+          route: "http://10.0.2.2:5000/submit"),
+      detailRoute: 'http://10.0.2.2:5000/detail',
+      label: 'Cat Dog Classification'),
   Model(
-    label: 'Flower classification',
-    method: 'POST',
-    route: 'http://10.0.2.2:5001/submit',
-    key: 'img',
-    description:
-        'classify 5 types of flower including dandelion, daisy, sunflower, tulip and rose',
-  ),
-  Model(
-    label: 'Leaf classification',
-    method: 'POST',
-    route: 'https://test.com/proj/test/leaf',
-    key: 'img',
-    description:
-        'leaf classification test, just simply add a photo of a leaf and wait for the result!!',
-  ),
-  Model(
-    label: 'Bottle',
-    method: 'POST',
-    route: 'https://test.com/proj/test/bottle',
-    key: 'img',
-    description: 'bottle test',
-  ),
-  Model(
-    label: 'OCR',
-    method: 'POST',
-    route:
-        'https://api.ocr.space/parse/image?apikey=3cc8d9406188957',
-    key: 'img',
-    description: 'free ocr',
-  )
+      detail: Detail(
+          description:
+              "classify 5 types of flower including dandelion, daisy, sunflower, tulip and rose",
+          imgType: "file",
+          key: "img",
+          method: "POST",
+          route: "http://10.0.2.2:5001/submit"),
+      detailRoute: 'http://10.0.2.2:5001/detail',
+      label: 'Flower Classification'),
+  // Model(
+  //   label: 'Cat Dog classification',
+  //   method: 'POST',
+  //   route: 'http://10.0.2.2:5000/submit',
+  //   key: 'img',
+  //   description: 'Cat and Dog classification',
+  // ),
+  // Model(
+  //   label: 'Flower classification',
+  //   method: 'POST',
+  //   route: 'http://10.0.2.2:5001/submit',
+  //   key: 'img',
+  //   description:
+  //       'classify 5 types of flower including dandelion, daisy, sunflower, tulip and rose',
+  // ),
+  // Model(
+  //   label: 'Leaf classification',
+  //   method: 'POST',
+  //   route: 'https://test.com/proj/test/leaf',
+  //   key: 'img',
+  //   description:
+  //       'leaf classification test, just simply add a photo of a leaf and wait for the result!!',
+  // ),
+  // Model(
+  //   label: 'Bottle',
+  //   method: 'POST',
+  //   route: 'https://test.com/proj/test/bottle',
+  //   key: 'img',
+  //   description: 'bottle test',
+  // ),
+  // Model(
+  //   label: 'OCR',
+  //   method: 'POST',
+  //   route:
+  //       'https://api.ocr.space/parse/image?apikey=3cc8d9406188957',
+  //   key: 'img',
+  //   description: 'free ocr',
+  // )
 ];
--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,15 +1,101 @@
-class Model {
-  final String label;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Detail {
   final String method;
   final String route;
   final String key;
-  final String? description;
+  final String imgType;
+  final String description;
 
-  const Model({
-    required this.label,
+  Detail({
     required this.method,
     required this.route,
     required this.key,
-    this.description,
+    required this.imgType,
+    required this.description,
   });
+
+  factory Detail.fromJson(dynamic json) {
+    return Detail(
+      method: json['method'] as String,
+      route: json['route'] as String,
+      key: json['key'] as String,
+      imgType: json['imgType'] as String,
+      description: json['description'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'method': method,
+      'route': route,
+      'key': key,
+      'imgType': imgType,
+      'description': description,
+    };
+  }
+
+  static Detail fromMap(Map<String, dynamic> map) {
+    return Detail(
+      method: map['method'],
+      route: map['route'],
+      key: map['key'],
+      imgType: map['imgType'],
+      description: map['description'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Detail { method: $method, route: $route, key: $key, imgType: $imgType, description: $description}';
+  }
+}
+
+class Model {
+  final Detail detail;
+  final String detailRoute;
+  final String label;
+
+  const Model({
+    required this.detail,
+    required this.detailRoute,
+    required this.label,
+  });
+
+  factory Model.fromJson(dynamic json) {
+    return Model(
+      detail: json['detail'] as Detail,
+      detailRoute: json['detailRoute'] as String,
+      label: json['label'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'detail': detail,
+      'detailRoute': detailRoute,
+      'label': label,
+    };
+  }
+
+  static Model fromMap(Map<String, dynamic> map) {
+    return Model(
+      detail: map['detail'],
+      detailRoute: map['detailRoute'],
+      label: map['label'],
+    );
+  }
+
+  factory Model.fromDocument(DocumentSnapshot documentSnapshot) {
+    return Model(
+      detail: documentSnapshot['detail'],
+      detailRoute: documentSnapshot['detailRoute'],
+      label: documentSnapshot['label'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Detail { detail: $detail, detailRoute: $detailRoute, label: $label}';
+  }
 }
